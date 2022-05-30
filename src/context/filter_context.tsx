5bfa827b9d7af45cfd909a76a00e2038e9a7fa4e
ChukwukaHonour <ChukwukaHonour@gmail.com>
2022-05-30 07:42:42 +0100

    setting up all the context

---
+++ src/context/filter_context.tsx
@@ -0,0 +1,28 @@
+import React, { useEffect, useContext, useReducer } from 'react'
+import reducer from './reducers/filter_reducer';
+import {
+  LOAD_PRODUCTS,
+  SET_GRIDVIEW,
+  SET_LISTVIEW,
+  UPDATE_SORT,
+  SORT_PRODUCTS,
+  UPDATE_FILTERS,
+  FILTER_PRODUCTS,
+  CLEAR_FILTERS,
+} from './actions'
+
+type CartProviderProps = {
+    children: React.ReactNode
+}
+const initialState = {}
+
+const FilterContext = React.createContext("Default");
+
+export const FilterProvider = ({children}: CartProviderProps) => {
+    return (
+        <FilterContext.Provider value="cart_context">{children}</FilterContext.Provider>
+    )
+}   
+export const useCartContext = () => {
+  return useContext(FilterContext)
+}
